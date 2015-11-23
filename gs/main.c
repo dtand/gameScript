286e3d2eb3119ba7d294a441d83230f94d341293
Daniel Anderson <danielanderson@Daniels-MacBook-Pro-3.local>
2015-11-23 16:15:09 -0500

    Initial Commit

---
+++ gs/main.c
@@ -0,0 +1,15 @@
+//
+//  main.c
+//  gs
+//
+//  Created by Daniel Anderson on 11/23/15.
+//  Copyright (c) 2015 Daniel Anderson. All rights reserved.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
